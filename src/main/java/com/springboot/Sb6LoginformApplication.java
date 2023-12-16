1a07cc663395bbe51d2990952e1f6e6c93fbef81
Istkaar <istkaar.ali2022@gmail.com>
2023-12-16 12:57:09 +0530

    Login Form

---
+++ src/main/java/com/springboot/Sb6LoginformApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb6LoginformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb6LoginformApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboot/controller/MyController.java
@@ -0,0 +1,39 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.springboot.entities.LoginData;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/form")
+	public String openForm(Model model)
+	{
+		System.out.println("Login Form");
+		model.addAttribute("loginData", new LoginData());
+		return "form";
+	}
+	
+	
+	
+	// handler for process form
+	@PostMapping("/process")
+	public String processForm(@Valid @ModelAttribute("loginData") LoginData loginData, BindingResult result)
+	{
+		if(result.hasErrors()) 
+		{
+			System.out.println(result);
+			return "form";
+		}
+		System.out.println("Login Data");
+		return "success";
+	}
+}
